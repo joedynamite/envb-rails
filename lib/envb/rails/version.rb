ea0e62e6651d6740b74224194ebbf65608bb68d7
Joe Bilt <code@joebilt.com>
2013-04-24 13:47:11 -0400

    html:after doesn't behave in Safari

--- lib/envb/rails/version.rb
+++ lib/envb/rails/version.rb
@@ -2,6 +2,6 @@ module Envb
   module Rails
     # Public: Current version of the envb-rails gem, tries to adhere to the
     # Semantic Versioning Specification.
-    VERSION = '0.0.4'
+    VERSION = '0.0.5'
   end
 end
